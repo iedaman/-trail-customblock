2993285f98949bdbec1d55d9e20414780a029be7
ieda <33334133+iedaman@users.noreply.github.com>
2023-12-07 15:38:23 +0900

    big change .  blink block is changed.  add main blocks

--- custom.ts
+++ custom.ts
@@ -3,36 +3,22 @@
 * このファイルを使って、独自の関数やブロックを定義してください。
 * 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
 */
-
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
 /**
- * Custom blocks
+ * Animation blocks
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace custom {
+namespace animations {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Blink an LED at a particular interval
+     * @param x horizontal led coordinate
+     * @param y vartical led coordinate
+     * @param interval time in milli seconds between blinks
      */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% block="blink|x $x |y $y |every $interval |ms"
+    //% x.min=0 x.max=4 y.min=0 y.max=4 interval.min=0 x.defl=2 y.defl=2 interval.defl=100
+    export function blink(x:number, y:number, interval:number): void {
+        let sprite = game.createSprite(x,y)
+        sprite.setBlink(interval)
+        sprite.blink()
     }
 }
--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+let i = 0
+basic.forever(function () {
+    i = 0
+    for (let index = 0; index < 4; index++) {
+        animations.blink(i, i, 100 + i * 100)
+        i += 1
+    }
+})
